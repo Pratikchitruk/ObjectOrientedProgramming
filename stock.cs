977b2cc4bb75f4683e69a786befd4dbfad7a5abd
Pratikchitruk <99415272+Pratikchitruk@users.noreply.github.com>
2022-03-10 15:21:15 +0530

    stock.cs push for oop

---
+++ stock.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockAccountManagnent
+{
+    public class MyStocks
+    {
+        private String companyName;
+        private int stockRating;
+        private string price;
+        private int numberOfShares;
+
+        public String getCompanyName()
+        {
+            return companyName;
+        }
+
+        public int getStockRating()
+        {
+            return stockRating;
+        }
+
+        public String getPrice()
+        {
+            return price;
+        }
+
+        public int getNumberOfShares()
+        {
+            return numberOfShares;
+        }
+
+        public  void Stock(String companyName, int stockRating, string price, int numberOfShares)
+        {
+            
+            this.companyName = companyName;
+            this.stockRating = stockRating;
+            this.price = price;
+            this.numberOfShares = numberOfShares;
+        }
+
+        internal static void Stock()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
