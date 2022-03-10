a0b52583427225e2b2c28e617d0e05c4ba31389c
PratikChitruk <pratik.chitruk@gmail.com>
2022-03-10 15:18:31 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome to stock managnemt program");
+
+        StockAccountManagnent.MyStocks.Stock();
+             
+
+        
+    }
+}
